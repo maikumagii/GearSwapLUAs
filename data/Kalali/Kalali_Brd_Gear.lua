31e06152dca6e33392c6b2bcb7aafc7db37de5ae
Michael McGee <>
2026-04-20 11:28:40 -0400

    Added DW to BRD til I get actual DW

--- data/Kalali/Kalali_Brd_Gear.lua
+++ data/Kalali/Kalali_Brd_Gear.lua
@@ -6,6 +6,7 @@ function character_user_job_setup()
     state.IdleMode:options('Normal', 'NoRefresh', 'DT')
     state.Weapons:options('None', 'Naegling', 'Aeneas', 'DualWeapons', 'DualNaegling', 'DualTauret', 'DualAeolian')
 
+    gear.melee_dw_back = {}
     gear.melee_str_wsd_back = { name = "Intarabus's Cape", augments = { 'STR+20', 'Accuracy+20 Attack+20', 'Weapon skill damage +10%', } }
     gear.melee_chr_wsd_back = gear.melee_str_wsd_back
     gear.melee_dex_wsd_back = gear.melee_str_wsd_back
@@ -15,10 +16,8 @@ function character_user_job_setup()
 
     gear.linos_str_wsd = { name = "Linos", augments = { 'STR+8', 'Attack+15', 'Weapon skill damage +2%' } } -- Savage Blade
     gear.linos_chr_wsd = { name = "Linos", augments = { 'CHR+8', 'Attack+20', 'Weapon skill damage +3%' } } -- Mordant Rime
-    gear.linos_dex_wsd = gear
-        .linos_chr_wsd                                                                                      -- Rudra's Storm
-    gear.linos_int_wsd = gear
-        .linos_chr_wsd                                                                                      -- Aeolian Edge
+    gear.linos_dex_wsd = gear.linos_chr_wsd                                                                                      -- Rudra's Storm
+    gear.linos_int_wsd = gear.linos_chr_wsd                                                                                      -- Aeolian Edge
     gear.linos_fc = gear.linos_chr_wsd
     gear.linos_tp = { name = "Linos", augments = { 'Accuracy+19', '"Store TP"+4', 'Quadruple Attack +3%' } }
     -- JSE Weapon/Neck/Earring
@@ -657,14 +656,14 @@ function init_gear_sets()
         range = gear.linos_tp,
         head = "Aya. Zucchetto +2",
         neck = "Bard's Charm +2",
-        ear1 = "Cessance Earring",
+        ear1 = "Eabani Earring",
         ear2 = "Telos Earring",
         body = "Ayanmo Corazza +2",
         hands = "Bunzi's Gloves",
         ring1 = { name = "Moonlight Ring", bag = "Wardrobe" },
         ring2 = { name = "Moonlight Ring", bag = "Wardrobe 2" },
         back = "Null Shawl",
-        waist = "Null Belt",
+        waist = "Reiki Yotai",
         legs = "Volte Tights",
         feet = "Nyame Sollerets"
     }
